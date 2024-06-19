04269c7640755b1bd08b225fb9827aca770d2e15
MaxKing <1512347620@qq.com>
2024-06-19 18:34:32 +0800

    .

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.maxking.icu": dockerHub,
+  "quay.maxking.icu": "https://quay.io",
+  "gcr.maxking.icu": "https://gcr.io",
+  "k8s-gcr.maxking.icu": "https://k8s.gcr.io",
+  "k8s.maxking.icu": "https://registry.k8s.io",
+  "ghcr.maxking.icu": "https://ghcr.io",
+  "cloudsmith.maxking.icu": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.maxking.icu": dockerHub,
 };
 
 function routeByHosts(host) {
